c242ba8cfc9b04b2d153bc7068072df64d52a0ae
ramirezfred <ramirez.fred@hotmail.com>
2018-02-09 22:00:19 -0430

    ruta para picking departament

--- tecprecincComprasAPI/app/Http/Controllers/PedidoController.php
+++ tecprecincComprasAPI/app/Http/Controllers/PedidoController.php
@@ -266,7 +266,7 @@ class PedidoController extends Controller
         $picking = json_decode($request->input('picking'));
         //$picking = $request->input('picking');
 
-        return response()->json(['picking'=>$picking],200);
+        return response()->json(['picking'=>$picking->categoria->tipo->nombre],200);
 
         if ($picking->categoria->tipo->nombre == 'CONSUMO') {
             
